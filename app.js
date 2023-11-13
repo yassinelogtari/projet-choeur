517d9d954b149e8745fe1fd2d0d7ba8512f42148
yassinelogtari <logtari.yassine31@gmail.com>
2023-11-13 17:55:03 +0100

    squelette-commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ("express")
+const mongoose=require("mongoose")
+
+
+
+
+
+mongoose
+  .connect(
+   "mongodb://127.0.0.1:27017/choeur-project"
+  )
+  .then(console.log("connected to mongodb"))
+  .catch((err) => console.log(err));
+
+const app = express()
+app.use(express.json())
+
+
+
+module.exports = app
