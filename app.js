bf799b2679d338b0428abd96e6b47942844b6fcf
mohamed ounissi <ounissilol@gmail.com>
2023-11-13 18:06:27 +0100

    personal second commit

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-const express = require ("express")
-const mongoose=require("mongoose")
-
-
-
-
+const express = require("express");
+const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-   "mongodb://127.0.0.1:27017/choeur-project"
-  )
+  .connect("mongodb://127.0.0.1:27017/choeur-project")
   .then(console.log("connected to mongodb"))
   .catch((err) => console.log(err));
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+console.log("test");
 
-
-
-module.exports = app
+module.exports = app;
