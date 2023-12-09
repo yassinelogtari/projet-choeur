158570338e545e1ed920c73fe6ef17132d9894e6
amenilallahem <ameni.lallahem19@gmail.com>
2023-12-09 11:02:53 +0100

    saisir les informations sur l'audition du candidat

--- models/auditionModel.js
+++ models/auditionModel.js
@@ -5,6 +5,7 @@ const auditionSchema = new mongoose.Schema(
     candidats: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: "candidat",
+      
     }],
 
     DateAud: {
@@ -27,6 +28,34 @@ const auditionSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    candidatsInfo: [
+      {
+        candidat: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "candidat",
+        },
+        extraitChante: {
+          type: String,
+          default: false,
+        },
+        tessiture: {
+          type: String,
+          default: false,
+        },
+        evaluation: {
+          type: String,
+          enum: ["A", "B", "C"], 
+          default: false,
+        },
+        decision: {
+          type: String,
+          enum: ["Accepté", "Refusé", "En attente"]
+        },
+        remarque: {
+          type: String,
+        }
+      },
+    ],
   },
   { timestamps: true }
 );
