5c22b288dededa6584fa653a17c3e5704613e0a5
mohamed ounissi <ounissilol@gmail.com>
2023-12-10 08:26:55 +0100

    added the additional audition planning for the failingCandidats

--- controllers/auditionController.js
+++ controllers/auditionController.js
@@ -1,5 +1,5 @@
 const Joi = require("joi");
-
+const mongoose = require("mongoose");
 const sendEmail = require("../utils/sendEmail");
 const Audition = require("../models/auditionModel");
 const Candidats = require("../models/candidatModel");
@@ -64,6 +64,7 @@ const generateSchedule = async (req, res) => {
     const { startDate, CandidatsPerHour, startTime, endTime } = req.body;
 
     const candidats = await Candidats.find({});
+    console.log(candidats);
 
     if (candidats.length === 0) {
       return res.status(400).json({
@@ -101,7 +102,106 @@ const generateSchedule = async (req, res) => {
     if (savedAuditions) {
       for (let i = 0; i < savedAuditions.length; i++) {
         for (let j = 0; j < savedAuditions[i].candidats.length; j++) {
-          console.log(savedAuditions[i].candidats[j]);
+          const candidate = await Candidats.findById(
+            savedAuditions[i].candidats[j]
+          );
+
+          const formattedDateString =
+            savedAuditions[i].DateAud.toISOString().slice(8, 10) +
+            "-" +
+            (savedAuditions[i].DateAud.getUTCMonth() + 1)
+              .toString()
+              .padStart(2, "0") +
+            "-" +
+            savedAuditions[i].DateAud.getUTCFullYear();
+          const formattedTimeDString = savedAuditions[
+            i
+          ].HeureDeb.toISOString().slice(11, 16);
+          const formattedTimeFString = savedAuditions[
+            i
+          ].HeureFin.toISOString().slice(11, 16);
+
+          await sendEmail(
+            candidate.email,
+            "Audition Information",
+            "Bonjour " +
+              candidate.prenom +
+              " Félicitations ! Vous avez été présélectionné pour rejoindre l'orchestre symphonique de Cartage. Votre audition aura lieu à " +
+              formattedDateString +
+              " à partir de " +
+              formattedTimeDString +
+              " à " +
+              formattedTimeFString +
+              ". nous avons hâte de découvrir vos talents et de vous voir rejoindre notre équipe."
+          );
+        }
+      }
+      res.status(201).send({
+        message:
+          "Audition est affectée avec succès et les Email ont été envoyés avec succès",
+        dataA: savedAuditions,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, msg: error.message });
+  }
+};
+
+const generateAdditionalSchedule = async (req, res) => {
+  try {
+    const { error } = Joi.object({
+      startDate: Joi.date().required(),
+      CandidatsPerHour: Joi.number().integer().required(),
+      startTime: Joi.number().integer().required(),
+      endTime: Joi.number().integer().required(),
+      failingCandidats: Joi.array().items(Joi.string()).required(),
+    }).validate(req.body, { abortEarly: false });
+
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        msg: `Validation error: ${error.details
+          .map((detail) => detail.message)
+          .join(", ")}`,
+      });
+    }
+
+    const {
+      startDate,
+      CandidatsPerHour,
+      startTime,
+      endTime,
+      failingCandidats,
+    } = req.body;
+
+    const candidats = failingCandidats.map(
+      (id) => new mongoose.Types.ObjectId(id)
+    );
+
+    const schedule = generateAuditionSchedule(
+      startDate,
+      candidats,
+      CandidatsPerHour,
+      startTime,
+      endTime
+    );
+
+    const auditionInstances = schedule.map(
+      (session) =>
+        new Audition({
+          candidats: session.candidates,
+          DateAud: new Date(session.date),
+          HeureDeb: new Date(session.startTime),
+          HeureFin: new Date(session.endTime),
+        })
+    );
+
+    const savedAuditions = await Audition.insertMany(auditionInstances);
+
+    if (savedAuditions) {
+      for (let i = 0; i < savedAuditions.length; i++) {
+        for (let j = 0; j < savedAuditions[i].candidats.length; j++) {
           const candidate = await Candidats.findById(
             savedAuditions[i].candidats[j]
           );
@@ -159,7 +259,15 @@ const fetshAuditions = async (req, res) => {
 
 const addAuditionInfo = async (req, res) => {
   try {
-    const { auditionId, candidatId, extraitChante, tessiture, evaluation, decision, remarque } = req.body;
+    const {
+      auditionId,
+      candidatId,
+      extraitChante,
+      tessiture,
+      evaluation,
+      decision,
+      remarque,
+    } = req.body;
 
     const { error } = Joi.object({
       auditionId: Joi.string().required(),
@@ -169,44 +277,81 @@ const addAuditionInfo = async (req, res) => {
       evaluation: Joi.string().valid("A", "B", "C").required(),
       decision: Joi.string(),
       remarque: Joi.string(),
-    }).validate({ auditionId, candidatId, extraitChante, tessiture, evaluation, decision, remarque }, { abortEarly: false });
+    }).validate(
+      {
+        auditionId,
+        candidatId,
+        extraitChante,
+        tessiture,
+        evaluation,
+        decision,
+        remarque,
+      },
+      { abortEarly: false }
+    );
 
     if (error) {
       return res.status(400).json({
         success: false,
-        msg: `Validation error: ${error.details.map((detail) => detail.message).join(", ")}`,
+        msg: `Validation error: ${error.details
+          .map((detail) => detail.message)
+          .join(", ")}`,
       });
     }
 
     const audition = await Audition.findById(auditionId);
 
     if (!audition) {
-      return res.status(404).json({ success: false, msg: "Audition not found." });
+      return res
+        .status(404)
+        .json({ success: false, msg: "Audition not found." });
     }
 
     if (!audition.candidats.includes(candidatId)) {
-      return res.status(400).json({ success: false, msg: "Candidate not associated with this audition." });
+      return res.status(400).json({
+        success: false,
+        msg: "Candidate not associated with this audition.",
+      });
     }
 
     audition.candidatsInfo = audition.candidatsInfo || [];
 
-  
-    const candidatInfoIndex = audition.candidatsInfo.findIndex(info => info && info.candidat && info.candidat.toString() === candidatId);
+    const candidatInfoIndex = audition.candidatsInfo.findIndex(
+      (info) => info && info.candidat && info.candidat.toString() === candidatId
+    );
 
     if (candidatInfoIndex !== -1) {
-      audition.candidatsInfo[candidatInfoIndex] = { extraitChante, tessiture, evaluation, decision, remarque };
+      audition.candidatsInfo[candidatInfoIndex] = {
+        extraitChante,
+        tessiture,
+        evaluation,
+        decision,
+        remarque,
+      };
     } else {
-      audition.candidatsInfo.push({ extraitChante, tessiture, evaluation, decision, remarque });
+      audition.candidatsInfo.push({
+        extraitChante,
+        tessiture,
+        evaluation,
+        decision,
+        remarque,
+      });
     }
 
     await audition.save();
 
-    res.status(200).json({ success: true, msg: "Audition information added successfully." });
+    res
+      .status(200)
+      .json({ success: true, msg: "Audition information added successfully." });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, msg: error.message });
   }
 };
 
-
-module.exports = { generateSchedule, fetshAuditions,addAuditionInfo };
+module.exports = {
+  generateSchedule,
+  fetshAuditions,
+  addAuditionInfo,
+  generateAdditionalSchedule,
+};
--- routes/auditionRoute.js
+++ routes/auditionRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auditionController = require("../controllers/auditionController");
 
 router.post("/generate", auditionController.generateSchedule);
+router.post("/generate/additional", auditionController.generateAdditionalSchedule);
 
 router.get("/", auditionController.fetshAuditions);
 router.post("/addinfo", auditionController.addAuditionInfo);
