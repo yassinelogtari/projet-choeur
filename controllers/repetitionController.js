54d5975aabe2ca98ac99344929ec492e64b5857e
mohamed ounissi <ounissilol@gmail.com>
2023-12-11 18:07:43 +0100

    handling presence for members (cancert/repetion) + generating qr code ( front end part not handeled yet )

---
+++ controllers/repetitionController.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const Repetition = require("../models/repetitionModel");
+const addQrCodeToRepetition = require("../middlewares/createQrCodeMiddleware");
+
+const createRepetition = async (req, res) => {
+  try {
+    const newRepetition = new Repetition(req.body);
+    await newRepetition.save();
+    req.repetitionId = newRepetition._id;
+    await addQrCodeToRepetition.addQrCodeToRepetition(req, res, () => {});
+  } catch (error) {
+    console.error("Error creating repetition:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = {createRepetition};
