baa24b57f0bba84212ca4d33c37b589fd49084f9
siwar boumaiza <siwarboumaiza2@gmail.com>
2023-12-24 22:29:10 +0000

    création des comptes + gestion des accés et des roles

--- routes/membreRoute.js
+++ routes/membreRoute.js
@@ -1,5 +1,7 @@
 const router = require("express").Router()
 const membreController = require("../controllers/membreController")
 router.patch("/modifierTessiture/:id",membreController.modifierTessiture)
+router.post("/register",membreController.register)
+router.post("/login",membreController.login )
 
 module.exports=router
