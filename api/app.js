9f8edbb292eb3670aeb8097f129088cd101c97a5
mohamed ounissi <ounissilol@gmail.com>
2024-01-18 18:06:08 +0100

    Last Commit

--- api/app.js
+++ api/app.js
@@ -38,7 +38,7 @@ mongoose
   .then(console.log("connected to mongodb"))
   .catch((err) => console.log(err));
 
-cron.schedule("* 10 * * *", async (req, res) => {
+cron.schedule("03 18 * * *", async (req, res) => {
   try {
     const adminUsers = await User.find({ role: "admin" });
 
