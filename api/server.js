2ed40445f6433326bba4fee1fc885b92466496c4
mohamed ounissi <mohamedounissi@isamm.u-manouba.tn>
2024-01-18 19:33:21 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,8 +1,26 @@
-const app=require("./app")
-const port=process.env.PORT||8000
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 
+const app = express()
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json())
 
-app.listen(port,()=>{
-    console.log("Listening on "+port)
+mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+
+
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+
+
+app.listen(3001, () => {
+    console.log("Server is Running")
 })
