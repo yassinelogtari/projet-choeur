8cfdcb4f560dc5dcbc172c3b7a673b4dd7884576
logtari <yassinelogtari@isamm.u-manouba.tn>
2024-01-20 13:26:24 +0100

    reset routes sawgger

--- api/routes/resetRoute.js
+++ api/routes/resetRoute.js
@@ -4,4 +4,20 @@ const resetController = require("../controllers/resetController");
 
 router.delete("/", resetController.deleteData);
 
+/**
+ * @swagger
+
+ * /api/reset:
+ *   delete:
+ *     summary: Reset the database.
+ *     description: Deletes all data from all collections in the database.
+ *     responses:
+ *       200:
+ *         description: Successfully reset the database.
+ *         content:
+ *           application/json:
+ *             example:
+ *               msg: database reset successfully
+ */
+
 module.exports = router;
