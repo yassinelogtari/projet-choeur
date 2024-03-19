b857dbd4cce43430b3b097398c7cf9a274db7fb3
mohamed ounissi <ounissilol@gmail.com>
2024-03-19 01:19:59 +0100

    first commit front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,46 @@ import Login from "./routes/Login";
 import PresenceConcert from "./routes/PresenceConcert";
 import PresenceRepetition from "./routes/PresenceRepetititon";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./assets/css/demo.css";
+import "./assets/vendor/css/core.css";
+import "./assets/vendor/css/theme-default.css";
+import AdminDashboard from "./pages/AdminDashboard";
+import ChefPupitreDashboard from "./pages/ChefPupitreDashboard";
+import ChoristeDashboard from "./pages/ChoristeDashboard";
+import ManagerDashboard from "./pages/ManagerDashboard";
 
 const App = () => {
-  
   return (
     <Router>
       <>
         <Routes>
+          <Route
+            exact
+            path="/dashboard/manager"
+            element={<ManagerDashboard />}
+          />
+          <Route
+            exact
+            path="/dashboard/choriste"
+            element={<ChoristeDashboard />}
+          />
+          <Route
+            exact
+            path="/dashboard/chef-de-pupitre"
+            element={<ChefPupitreDashboard />}
+          />
+          <Route exact path="/dashboard/admin" element={<AdminDashboard />} />
           <Route exact path="/" element={<Login />} />
-          <Route exact path="/presence/cancert/:idC" element={<PresenceConcert />} />
-          <Route exact path="/presence/repetition/:idR" element={<PresenceRepetition />} />
-          
+          <Route
+            exact
+            path="/presence/cancert/:idC"
+            element={<PresenceConcert />}
+          />
+          <Route
+            exact
+            path="/presence/repetition/:idR"
+            element={<PresenceRepetition />}
+          />
         </Routes>
       </>
     </Router>
