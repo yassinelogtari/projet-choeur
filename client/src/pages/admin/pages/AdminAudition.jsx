1e5890522cada7a5f5f73ca4927f0b149d68900d
logtari <yassinelogtari@isamm.u-manouba.tn>
2024-03-20 15:11:22 +0100

    actionButton-edit-add-delete

--- client/src/pages/admin/pages/AdminAudition.jsx
+++ client/src/pages/admin/pages/AdminAudition.jsx
@@ -3,23 +3,36 @@ import Table from "../../../components/table/Table";
 import { Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { NavLink } from "react-router-dom";
-
+import DeleteIcon from "@mui/icons-material/Delete";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
 const AdminAudition = () => {
   return (
     <div className="position-absolute top-50 start-50 translate-middle auditionTable">
       <p className="auditionTableTitle">Audition List</p>
-      <NavLink
-                  to="/dashboard/admin/addAudition"
-                  className="auditionButton"
-                >
-                  <Button type="submit" variant="contained">
-                    <div className="auditionButtonWords">
-                      <AddIcon />
-                      Audition Info
-                    </div>
-                  </Button>
-                </NavLink>
-      <Table />
+      <h6>
+        el buttonet hethom tanech t7othom fi west el action fil tabelau hama
+        wala souu
+      </h6>
+      <div className="auditionAction">
+        <NavLink to="/dashboard/admin/addAudition">
+          <Button variant="outlined" className="AddInfoAction">
+            <div>
+              <AddIcon />
+            </div>
+          </Button>
+        </NavLink>
+        <Button variant="outlined" className="editAction">
+          <div>
+            <ModeEditIcon className="editIcon" />
+          </div>
+        </Button>
+        <Button variant="outlined" className="deleteAction">
+          <div>
+            <DeleteIcon className="deleteIcon" />
+          </div>
+        </Button>
+        <Table />
+      </div>
     </div>
   );
 };
